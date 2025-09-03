8b370a0db3d5d71492ba8afc167e22151d4e86b2
Sarthak Bukane (sarry) <113605836+Sarry000@users.noreply.github.com>
2025-09-03 19:00:17 +0530

    Update about.tsx

--- src/components/sections/about.tsx
+++ src/components/sections/about.tsx
@@ -33,7 +33,7 @@ export function About() {
             <div className="relative group">
               <div className="absolute -inset-1.5 bg-gradient-to-r from-accent to-primary rounded-full opacity-50 blur-xl group-hover:opacity-75 transition duration-500"></div>
               <Image
-                src="/profile.png"
+                src="/profile2.png"
                 alt="Sarthak"
                 width={400}
                 height={400}
